4dcf3582cf808c282b296fd4b88cb9656112dc88
Elisha Benjamin <malishben360@gmail.com>
2023-04-15 02:55:05 +0100

    FEAT: Add user model and actions

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+export { 
+    getUsers,
+    getUserByEmail,
+    getUserBySessionToken,
+    getUserById,
+    createUser,
+    deleteUserById,
+    updateUserById
+} from './models/UserModel';
---
+++ src/db/models/UserModel.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+/** Fields with select false are not return by default
+ * Always specify during data fetch.
+ */
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    authentication: {
+        password: { type: String, required: true, select: false },
+        salt: { type: String, select: false },
+        sessionToken: { type: String, select: false }
+    }
+});
+
+export const UserModel = mongoose.model('User', UserSchema);
+
+/** User model actions */
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
+    'authentication.sessionToken': sessionToken
+});
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values)
+    .save().then((user) => user.toObject());
