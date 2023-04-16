2cfe74e43b962f616ec772f19c7e16c99fa65b00
Elisha Benjamin <malishben360@gmail.com>
2023-04-16 15:13:05 +0100

    FEAT: Add end point route for decode

--- src/router/v1/url.ts
+++ src/router/v1/url.ts
@@ -1,7 +1,8 @@
 import { isAuthenticated } from '../../middleware';
-import { encodeLongURL } from '../../controller/UrlController';
+import { encodeLongURL, decodeShortURL } from '../../controller/UrlController';
 import express from 'express';
 
 export default (router: express.Router) => {
     router.post('/encode', isAuthenticated, encodeLongURL);
+    router.post('/decode', isAuthenticated, decodeShortURL);
 }
